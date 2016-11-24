90a7dcf795ae296d4add09bc42a07a6b39fe104d
Raphael <raphaeloliveirinha015@gmail.com>
2016-11-23 23:17:07 -0200

    TESTE 5
    TESTE WORKSPACE SYNC

--- src/main.c
+++ src/main.c
@@ -2,15 +2,7 @@
 
 int main(void)
 {
-<<<<<<< HEAD
-	printf("teste 1\n");
-=======
-<<<<<<< HEAD
-	printf("hello");
-=======
-	printf("teste\n");
->>>>>>> bba02a82fa5368e1dcfd4171bd288adf85a011c5
->>>>>>> origin/master
+	printf("teste 2\n");
     return 0;
 }
 
